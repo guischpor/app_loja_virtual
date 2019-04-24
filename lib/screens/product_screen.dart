cbbe780614950dd5c0f6ec20533a9ad1e972fbe4
guilhermeportela <38728633+guischpor@users.noreply.github.com>
2019-04-24 17:21:51 -0300

    update

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_pro/carousel_pro.dart';
 import 'package:ecommerce_app/models/products_data.dart';
 import 'package:flutter/material.dart';
 
@@ -18,8 +19,32 @@ class _ProductScreenState extends State<ProductScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
+    final Color primaryColor = Theme.of(context).primaryColor;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(product.title),
+        centerTitle: true,
+      ),
+      body: ListView(
+        children: <Widget>[
+          AspectRatio(
+            aspectRatio: 0.9,
+            child: Carousel(
+              images: product.images.map((url) {
+                return NetworkImage(url);
+              }).toList(),
+              dotSize: 4.0,
+              dotBgColor: Colors.transparent,
+              dotColor: primaryColor,
+              dotSpacing: 15.0,
+              autoplay: false,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(16.0),
+          )
+        ],
+      ),
     );
   }
 }
