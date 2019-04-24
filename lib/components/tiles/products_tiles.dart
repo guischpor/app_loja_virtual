da5c30b59c7666016d135b268306180e49eef242
guilhermeportela <38728633+guischpor@users.noreply.github.com>
2019-04-23 21:31:11 -0300

    update

--- lib/components/tiles/products_tiles.dart
+++ lib/components/tiles/products_tiles.dart
@@ -6,13 +6,51 @@ class ProductTile extends StatelessWidget {
   //const ProductsTiles({Key key}) : super(key: key);
 
   final String type;
-  final ProductData data;
-  ProductTile(this.type, this.data);
+  final ProductData product;
+  ProductTile(this.type, this.product);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
+    return InkWell(
+      child: Card(
+        child: type == 'grid'
+            ? Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  AspectRatio(
+                    aspectRatio: 0.8,
+                    child: Image.network(
+                      product.images[0],
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      padding: EdgeInsets.all(8.0),
+                      child: Column(
+                        children: <Widget>[
+                          Text(
+                            product.title,
+                            style: TextStyle(fontWeight: FontWeight.w500),
+                          ),
+                          Text(
+                            'R\$ ${product.price.toStringAsFixed(2)}',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 17.0,
+                                color: Theme.of(context).primaryColor),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              )
+            : Row(
+                children: <Widget>[],
+              ),
+      ),
     );
   }
 }
