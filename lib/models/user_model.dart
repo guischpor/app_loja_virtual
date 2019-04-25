4a67603fcfe9bc910c1316c5528d1aa1a8f0cdac
guilhermeportela <38728633+guischpor@users.noreply.github.com>
2019-04-25 20:13:39 -0300

    update

---
+++ lib/models/user_model.dart
@@ -0,0 +1,17 @@
+import 'package:scoped_model/scoped_model.dart';
+
+class UserModel {
+  //usuario atual
+
+  //cadastro
+  void signUp() {}
+
+  //login
+  void signIn() {}
+
+  //esqueceu a senha
+  void recoverPass() {}
+
+  //manter usuario logado
+  bool isLoggedIn() {}
+}
