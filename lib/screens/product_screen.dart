9dbcdfe59ea026f010155550c779382f3c936c0d
guilhermeportela <38728633+guischpor@users.noreply.github.com>
2019-04-24 21:27:33 -0300

    update

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -14,6 +14,7 @@ class ProductScreen extends StatefulWidget {
 
 class _ProductScreenState extends State<ProductScreen> {
   final ProductData product;
+  String size;
 
   _ProductScreenState(this.product);
 
@@ -57,6 +58,45 @@ class _ProductScreenState extends State<ProductScreen> {
                       fontWeight: FontWeight.bold,
                       color: primaryColor),
                 ),
+                SizedBox(height: 16.0),
+                Text(
+                  "Tamanhos",
+                  style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w500),
+                ),
+                SizedBox(
+                  height: 34.0,
+                  child: GridView(
+                    scrollDirection: Axis.horizontal,
+                    padding: EdgeInsets.symmetric(vertical: 4.0),
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 1,
+                      mainAxisSpacing: 8.0,
+                      childAspectRatio: 0.5,
+                    ),
+                    children: product.sizes.map((s) {
+                      return GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            size = s;
+                          });
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(4.0)),
+                              border: Border.all(
+                                  color: s == size
+                                      ? primaryColor
+                                      : Colors.grey[500],
+                                  width: 3.0)),
+                          width: 50.0,
+                          alignment: Alignment.center,
+                          child: Text(s),
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                )
               ],
             ),
           ),
