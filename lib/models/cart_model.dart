f52a9bd2a8c1cf88a6d97c408b20bc83f13cc70d
guilhermeportela <38728633+guischpor@users.noreply.github.com>
2019-04-29 21:58:09 -0300

    update

---
+++ lib/models/cart_model.dart
@@ -0,0 +1,42 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:ecommerce_app/models/cart_products.dart';
+import 'package:ecommerce_app/models/user_model.dart';
+import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
+
+class CartModel extends Model {
+  //busca o usuario atual
+  UserModel user;
+
+  //cria a lista do carrinho de compras
+  List<CartProduct> products = [];
+
+  CartModel(this.user);
+
+  void addCartItem(CartProduct cartProduct) {
+    products.add(cartProduct);
+
+    Firestore.instance
+        .collection('users')
+        .document(user.firebaseUser.uid)
+        .collection('cart')
+        .add(cartProduct.toMap())
+        .then((doc) {
+      cartProduct.cid = doc.documentID;
+    });
+    notifyListeners();
+  }
+
+  void removeCartItem(CartProduct cartProduct) {
+    Firestore.instance
+        .collection('users')
+        .document(user.firebaseUser.uid)
+        .collection('cart')
+        .document(cartProduct.cid)
+        .delete();
+
+    products.remove(cartProduct);
+
+    notifyListeners();
+  }
+}
