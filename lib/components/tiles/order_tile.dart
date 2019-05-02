65365c53d8dd63c9c77b50034a544bf94636b07a
guilhermeportela <38728633+guischpor@users.noreply.github.com>
2019-05-02 20:25:16 -0300

    update

--- lib/components/tiles/order_tile.dart
+++ lib/components/tiles/order_tile.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
@@ -24,11 +26,16 @@ class OrderTile extends StatelessWidget {
               return Center(child: CircularProgressIndicator());
             else {
               return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   Text(
                     'Código do pedido: ${snapshot.data.documentID}',
                     style: TextStyle(fontWeight: FontWeight.bold),
                   ),
+                  SizedBox(
+                    height: 4.0,
+                  ),
+                  Text(_buildProductsText(snapshot.data)),
                 ],
               );
             }
@@ -36,5 +43,16 @@ class OrderTile extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  String _buildProductsText(DocumentSnapshot snapshot) {
+    String text = 'Descrição:\n';
+    for (LinkedHashMap p in snapshot.data['products']) {
+      text +=
+          "${p['quantity']} x ${p['product']['title']} (R\$ ${p['product']['price'].toStringAsFixed(2)})\n";
+    }
+
+    text += "Total: R\$ ${snapshot.data['totalPrice'].toStringAsFixed(2)}";
+    return text;
   }
 }
